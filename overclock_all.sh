1127f9165cae084a7b82cb17db6c4df80c284ce1
Łukasz Chachurski <lukasz.chachurski@baytekin.info>
2018-01-20 18:00:41 +0100

    reorder gpus

--- overclock_all.sh
+++ overclock_all.sh
@@ -22,20 +22,17 @@ sudo nvidia-smi -i 10 -pm 1
 sudo nvidia-smi -i 11 -pm 1
 sudo nvidia-smi -i 12 -pm 1
 
-
-# AMP GPUs
+# GPUs
 sudo /usr/bin/miner/overclock_one -g 0 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 1 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 2 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 3 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 4 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 5 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 6 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 7 -p 130 -m 1450 -c 100 -f 75
+sudo /usr/bin/miner/overclock_one -g 8 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 9 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 10 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 11 -p 130 -m 1450 -c 100 -f 75
 sudo /usr/bin/miner/overclock_one -g 12 -p 130 -m 1450 -c 100 -f 75
-
-# CORE GPUs
-sudo /usr/bin/miner/overclock_one -g 1 -p 130 -m 1450 -c 100 -f 75
-sudo /usr/bin/miner/overclock_one -g 2 -p 130 -m 1450 -c 100 -f 75
-sudo /usr/bin/miner/overclock_one -g 3 -p 130 -m 1450 -c 100 -f 75
-sudo /usr/bin/miner/overclock_one -g 5 -p 130 -m 1450 -c 100 -f 75
-sudo /usr/bin/miner/overclock_one -g 6 -p 130 -m 1450 -c 100 -f 75
-sudo /usr/bin/miner/overclock_one -g 8 -p 130 -m 1450 -c 100 -f 75
